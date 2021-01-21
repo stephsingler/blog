0a640dc3688fd4d151513830b3f0681b055ba169
stephsingler <Butterfly2825>
2021-01-20 20:05:26 -0600

    added text carousel

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.scss';
-import OverviewComponent from "../Overview/Overview";
-import FooterComponent from "../Footer/Footer";
-import InstagramFeedComponent from "../InstagramFeed/InstagramFeed";
+import OverviewComponent from '../Overview/Overview';
+import FooterComponent from '../Footer/Footer';
+import InstagramFeedComponent from '../InstagramFeed/InstagramFeed';
+import AnimatedSloganComponent from '../AnimatedSlogan/AnimatedSlogan';
 
 const AppComponent = () => {
   return (
     <div className="AppComponent">
       <OverviewComponent />
+      <AnimatedSloganComponent />
       <InstagramFeedComponent />
       <FooterComponent />
     </div>
